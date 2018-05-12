ad50e4247f343bb4118d1a386b346967fbb2ebfe
silvacarloss <ces.eduardo98@gmail.com>
2018-05-12 18:47:42 -0300

    Adição de imports necessários na classe de testes

---
+++ src/test/Controllers/UserTest.java
@@ -0,0 +1,11 @@
+package Controllers;
+
+import junit.framework.TestCase;
+import main.br.edu.ifsp.btv.Controllers.User;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class UserTest {
+
+}
