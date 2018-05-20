3c26fbaa50b619901962f4df4b9c3ccab93bc091
silvacarloss <ces.eduardo98@gmail.com>
2018-05-20 10:36:29 -0300

    Classe cash

---
+++ src/main/br/edu/ifsp/btv/Controllers/Cash.java
@@ -0,0 +1,13 @@
+package main.br.edu.ifsp.btv.Controllers;
+
+public class Cash {
+    private double cashTendered;
+
+    public double getCashTendered() {
+        return cashTendered;
+    }
+
+    public void setCashTendered(double cashTendered) {
+        this.cashTendered = cashTendered;
+    }
+}
